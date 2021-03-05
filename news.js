5df7629b1f755f97ea7cafce66ae3c1f265ed801
Nitin Rai <dvlnitins@gmail.com>
2021-03-05 20:28:07 +0530

    Update news.js

--- news.js
+++ news.js
@@ -6,13 +6,17 @@ let newsAccordion = document.getElementById('newsAccordion');
 
 // Create an ajax get request
 const xhr = new XMLHttpRequest();
-xhr.open('GET', `http://api.mediastack.com/v1/news?access_key=${apiKey}&sources=cnn,-bbc&languages=en`, true);
+
+xhr.withCredentials = true;
+xhr.open("GET", "https://newscatcher.p.rapidapi.com/v1/latest_headlines?lang=en&media=True");
+xhr.setRequestHeader("x-rapidapi-key", "a88be3b532msh436d515e78b0c7ap104f31jsnfccd6e30053c");
+xhr.setRequestHeader("x-rapidapi-host", "newscatcher.p.rapidapi.com");
 
 // What to do when response is ready
 xhr.onload = function () {
     if (this.status === 200) {
         let json = JSON.parse(this.responseText);
-        let articles = json.data;
+        let articles = json.articles;
         console.log(articles);
         let newsHtml = "";
         articles.forEach(function(element) {
@@ -21,16 +25,16 @@ xhr.onload = function () {
                             <div class="row">
                                 <div class="col-md-4 col-12">
                                 <img
-                                    src="${element["image"]}" 
+                                    src="${element["media"]}" 
                                     onerror="this.src = 'news.jpg';"
                                     class="img-fluid"
                                 />
                                 </div>
                                 <div class="col-md-8 col-12">
-                                    <small class="text-muted my-2">${element["published_at"]}</small>
+                                    <small class="text-muted my-2">${element["published_date"]}</small>
                                     <h4 class="card-title">${element["title"]}</h4>
-                                    <p class="card-text">${element["description"]}.. 
-                                    <span class="text-gradient"> <a href="${element['url']}" target="_blank" >Read</a> </span>
+                                    <p class="card-text">${element["summary"]}.. 
+                                    <span class="text-gradient"> <a href="${element['link']}" target="_blank" >Read</a> </span>
                                     </p>
                                 </div>
                             </div>
